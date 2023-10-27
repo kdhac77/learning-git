f5a77ee72b639a0e61f2a999a4ae4c288ea37469
Karolina Dabrowska <ikonygdansk@gmail.com>
2023-10-27 20:48:08 +0200

    Changing1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print("Liczby podzielne przez 5:")
-for number in range(1, 60):
+for number in range(1, 80):
     if number % 5 == 0:
         print(number)
 
 print("Liczby podniesione do potęgi 3:")
-for number in range(1, 105):
+for number in range(1, 76):
     result = number ** 3
     print(result)
